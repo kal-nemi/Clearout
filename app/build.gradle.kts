82a420031a4048b37a4a934784a9cf8c7be0dc9d
kal-nemi <vishal.mca20.du@gmail.com>
2026-05-25 03:06:33 +0530

    build(gradle): target Android 15 (API 35) & bump versionCode to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     defaultConfig {
         applicationId = "com.clearout.app"
         minSdk = 26
-        targetSdk = 34
-        versionCode = 2
+        targetSdk = 35
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
